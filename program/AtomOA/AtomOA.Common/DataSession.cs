703c7db0ddf2bc0151200dd75ad7526e5ddc08f9
唐富伟 <tianskyluj@sina.com>
2013-07-24 16:11:12 +0800

    添加公共类js样式

---
+++ program/AtomOA/AtomOA.Common/DataSession.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtomOA.Common
+{
+    public class DataSession
+    {
+        public DataSession()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+
+
+        // 销毁session 数据
+        public static void destroySession()
+        {
+            HttpContext.Current.Session.Clear();
+        }
+
+        //取得Session中的变量值
+        public static string getSessionValue(string Sessinname)
+        {
+            string _str = "";
+            try
+            {
+                _str = HttpContext.Current.Session[Sessinname].ToString();
+            }
+            catch
+            {
+
+            }
+            return _str;
+        }
+
+        //设置Session变量及值
+        public static void setSession(string Sessionname, object Sessionvalue)
+        {
+            HttpContext.Current.Session[Sessionname] = Sessionvalue;
+        }
+    }
+}
